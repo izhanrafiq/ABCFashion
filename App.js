8aeee322fb51b91294214259dba168dd64171021
Sathya <sathyasaikoukantla@gmail.com>
2021-09-04 16:42:22 +0530

    Final Navigation with all 5 Screens

--- App.js
+++ App.js
@@ -1,59 +1,49 @@
 import * as React from 'react';
-import {View, Text, ScrollView, SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from './components/Login';
-import Product from './components/Products/Index';
 import AddCart from './components/AddCart';
-
-function AboutScreen() {
-  return (
-    <View style={{alignItems: 'center', justifyContent: 'center'}}>
-      <Menu />
-      <Text>About Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{
-            headerStyle: {
-              backgroundColor: '#ff6969',
-            },
-            headerTintColor: 'white',
-          }}
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-          options={{
-            headerStyle: {
-              backgroundColor: '#ff6969',
-            },
-            headerTintColor: 'white',
-          }}
-          name="Product"
-          component={Product}
-        />
-        <Stack.Screen
-          options={{
-            headerStyle: {
-              backgroundColor: '#ff6969',
-            },
-            headerTintColor: 'white',
-          }}
-          name="Cart"
-          component={AddCart}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-    
+    <Navigation/>
   );
 }
+
 export default App;
+
+
+
+// const Stack = createStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           options={{
+//             headerStyle: {
+//               backgroundColor: '#ff6969'
+//             },
+//             headerTintColor: 'white'
+//           }}
+//           name="Login" component={Login} />
+
+//         <Stack.Screen
+//           options={{
+//             headerStyle: {
+//               backgroundColor: '#ff6969'
+//             },
+//             headerTintColor: 'white'
+//           }}
+//           name="Cart" component={AddCart} />
+//       <Stack.Screen name="Navigation" component={Navigation} />
+
+
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,1 +1,69 @@
-// hello
+import * as React from 'react';
+import { Image,FlatList,TouchableHighlight, SafeAreaView, StatusBar, Button,StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+import HomeScreen from './Screens/Home'
+import SearchScreen from './Screens/Search'
+import CartScreen from './Screens/Cart'
+import ProfileScreen from './Screens/Profile'
+import MoreScreen from './Screens/More'
+
+const homeName = 'Home'
+const searchName = 'Search'
+const cartName = 'Cart'
+const profileName = 'Profile'
+const moreName = 'More'
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === cartName) {
+              iconName = focused ? 'cart' : 'cart-outline';
+
+            } else if (rn === searchName) {
+              iconName = focused ? 'search' : 'search-outline';
+            } else if (rn === profileName) {
+              iconName = focused ? 'person' : 'person-outline';
+            } else if (rn === moreName) {
+              iconName = focused ? 'menu' : 'menu-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#FF6969',
+          inactiveTintColor: 'grey',
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70}
+        }}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={searchName} component={SearchScreen} />
+        <Tab.Screen name={cartName} component={CartScreen} />
+        <Tab.Screen name={profileName} component={ProfileScreen} />
+        <Tab.Screen name={moreName} component={MoreScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
